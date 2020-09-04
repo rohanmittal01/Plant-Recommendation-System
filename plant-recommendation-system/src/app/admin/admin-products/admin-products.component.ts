711418732d465444638813a1661a7a262c5fa042
rohanmittal01 <rohanmittal01@gmail.com>
2020-09-05 02:17:43 +0530

    Almost Completed Product Form, Admin Products Table

--- plant-recommendation-system/src/app/admin/admin-products/admin-products.component.ts
+++ plant-recommendation-system/src/app/admin/admin-products/admin-products.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { ProductService } from 'src/app/_services/product.service';
+import { AlertifyService } from 'src/app/_services/alertify.service';
+import { MatDialog } from '@angular/material/dialog';
+import { ActiveDialogComponent } from 'src/app/_dialogs/active-dialog/active-dialog.component';
 
 @Component({
   selector: 'app-admin-products',
@@ -7,9 +15,95 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminProductsComponent implements OnInit {
 
-  constructor() { }
+ // products$;
+ productListCollection: Observable<any[]>;
+ products: any = [];
+ filteredProducts: any[];
+ subscribe: Subscription;
 
-  ngOnInit(): void {
+ listData: MatTableDataSource<any>;
+ displayedColumns: string[] = ['sno', 'Common Name', 'Plant Type', 'price', 'isAvailable', '$key'];
+ @ViewChild(MatSort) sort: MatSort;
+ @ViewChild(MatPaginator) paginator: MatPaginator;
+ searchKey: string;
+ array: any;
+
+ constructor(private productService: ProductService, private alertify: AlertifyService, private dialog: MatDialog) {
+
+   this.productService.getAllProducts().subscribe(products => {
+     console.log(products);
+     this.array = products;
+     this.listData = new MatTableDataSource(this.array);
+     this.listData.sort = this.sort;
+     this.listData.paginator = this.paginator;
+ });
   }
+
+  applyFilter(){
+   this.listData.filter = this.searchKey.toLowerCase();
+ }
+ onSearchClear(){
+   this.searchKey = '';
+   this.applyFilter();
+ }
+
+ delete(id){
+   console.log(id);
+   console.log(this.array);
+   this.productService.delete(id).subscribe(x => {
+     for (const product in this.array){
+       // tslint:disable-next-line: triple-equals
+       if (this.array[product]._id == id){
+         this.array.splice(product, 1);
+         this.listData = new MatTableDataSource(this.array);
+         this.listData.sort = this.sort;
+         this.listData.paginator = this.paginator;
+         this.alertify.success('Product deleted successfully!');
+         return;
+       }
+     }
+   }, error => {
+     this.alertify.error('Product could not be deleted!');
+   });
+ }
+
+ modifyActive(id) {
+   console.log(this.array);
+   // tslint:disable-next-line: triple-equals
+   const modData = this.array.filter((x) => x._id == id);
+   modData[0].isAvailable = !modData[0].isAvailable;
+   console.log(modData[0]);
+   this.productService.update(id, modData[0]).subscribe(
+     () => {
+       this.alertify.success('Product Availability Modified Successfully');
+       // this.router.navigate(['/admin']);
+     },
+     (error) => {
+       this.alertify.error(error);
+     }
+   );
+ }
+
+ openDialog(id, e): void {
+   const dialogRef = this.dialog.open(ActiveDialogComponent);
+   dialogRef.afterClosed().subscribe((result) => {
+     console.log('Dialog Result: ' + result);
+     // tslint:disable-next-line: triple-equals
+     if (result == 'Yes') {
+       this.modifyActive(id);
+     // tslint:disable-next-line: triple-equals
+     } else if (result == 'No') {
+       // let modData = this.listData.filteredData.filter(x => x._id == id);
+       // console.log('role name: ' + modData[0].isActive);
+       // console.log("current: "+e);
+       // e = !e;
+       // console.log("current: "+e);
+       // modData = this.listData.filteredData.filter(x => x._id == id);
+     }
+   });
+ }
+
+ ngOnInit(): void {
+ }
 
 }
