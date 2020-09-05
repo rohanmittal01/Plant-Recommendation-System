4c0718731f066f240034901f03c028f89f98624d
rohanmittal01 <rohanmittal01@gmail.com>
2020-09-05 21:54:07 +0530

    Delivery person table and form

--- plant-recommendation-system/src/app/admin/admin-delivery-person/admin-delivery-person.component.ts
+++ plant-recommendation-system/src/app/admin/admin-delivery-person/admin-delivery-person.component.ts
@@ -1,15 +1,105 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActiveDialogComponent } from 'src/app/_dialogs/active-dialog/active-dialog.component';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
+import { Observable } from 'rxjs';
+import { DeliveryPersonService } from 'src/app/_services/delivery-person.service';
+import { MatDialog } from '@angular/material/dialog';
+import { AlertifyService } from 'src/app/_services/alertify.service';
 
 @Component({
   selector: 'app-admin-delivery-person',
   templateUrl: './admin-delivery-person.component.html',
   styleUrls: ['./admin-delivery-person.component.css']
 })
-export class AdminDeliveryPersonComponent implements OnInit {
+export class AdminDeliveryPersonComponent{
 
-  constructor() { }
+  deliveryListCollection: Observable<any[]>;
+  delivery: any = [];
+  filteredDelivery: any[];
 
-  ngOnInit(): void {
+  listData: MatTableDataSource<any>;
+  displayedColumns: string[] = [
+    'sno',
+    'name',
+    'datejoined',
+    'phoneNumber',
+    'isActive',
+    '$key',
+  ];
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  searchKey: string;
+  array: any;
+
+  constructor(
+    private deliveryService: DeliveryPersonService,
+    private dialog: MatDialog,
+    private alertify: AlertifyService
+  ) {
+    this.getDeliveryPerson();
+  }
+
+  getDeliveryPerson() {
+    this.deliveryService.getAll().subscribe((delivery) => {
+      console.log(delivery);
+      this.array = delivery;
+      console.log(this.array);
+      let data = this.array;
+      let d: any;
+      // tslint:disable-next-line: forin
+      for ( d in data ){
+        data[d].dateJoined = new Date(data[d].dateJoined).toLocaleString();
+        console.log(data[d].dateJoined);
+      }
+      console.log(data);
+      this.listData = new MatTableDataSource(data);
+      this.listData.sort = this.sort;
+      this.listData.paginator = this.paginator;
+    });
+  }
+
+  modifyActive(id) {
+    console.log(this.array);
+    // tslint:disable-next-line: triple-equals
+    const modData = this.array.filter((x) => x._id == id);
+    modData[0].isActive = !modData[0].isActive;
+    console.log(modData[0]);
+    this.deliveryService.update(id, modData[0]).subscribe(
+      () => {
+        this.alertify.success('Modified Successfully');
+        // this.router.navigate(['/admin']);
+      },
+      (error) => {
+        this.alertify.error(error);
+      }
+    );
+  }
+  openDialog(id, e): void {
+    const dialogRef = this.dialog.open(ActiveDialogComponent);
+    dialogRef.afterClosed().subscribe((result) => {
+      console.log('Dialog Result: ' + result);
+      // tslint:disable-next-line: triple-equals
+      if (result == 'Yes') {
+        this.modifyActive(id);
+      // tslint:disable-next-line: triple-equals
+      } else if (result == 'No') {
+        // let modData = this.listData.filteredData.filter(x => x._id == id);
+        // console.log('role name: ' + modData[0].isActive);
+        // console.log("current: "+e);
+        // e = !e;
+        // console.log("current: "+e);
+        // modData = this.listData.filteredData.filter(x => x._id == id);
+      }
+    });
+  }
+  applyFilter() {
+    this.listData.filter = this.searchKey.toLowerCase();
+  }
+  onSearchClear() {
+    this.searchKey = '';
+    this.applyFilter();
   }
 
 }
--- plant-recommendation-system/src/app/delivery-person-form/delivery-person-form.component.ts
+++ plant-recommendation-system/src/app/delivery-person-form/delivery-person-form.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActiveDialogComponent } from '../_dialogs/active-dialog/active-dialog.component';
+import { ActivatedRoute, Router } from '@angular/router';
+import { DeliveryPersonService } from '../_services/delivery-person.service';
+import { AlertifyService } from '../_services/alertify.service';
+import { MatDialog } from '@angular/material/dialog';
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: 'app-delivery-person-form',
@@ -7,9 +13,101 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeliveryPersonFormComponent implements OnInit {
 
-  constructor() { }
+  categories$: any;
+  deliveryperson: any = {};
+  address: any = {};
+  id;
 
-  ngOnInit(): void {
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private deliverypersonService: DeliveryPersonService,
+    private alertify: AlertifyService,
+    private dialog: MatDialog,
+    private authService: AuthService
+  ) {
+    this.id = this.route.snapshot.paramMap.get('id');
+    if (this.id) {
+      deliverypersonService.get(this.id).subscribe((categories: any) => {
+        this.deliveryperson = categories;
+        this.address = categories.address;
+        console.log(categories);
+      });
+    } else {
+      this.deliveryperson.isActive = true;
+    }
   }
 
+  ngOnInit(): void {}
+
+  save() {
+    this.deliveryperson = {
+      name: this.deliveryperson.name,
+      phoneNumber: this.deliveryperson.phoneNumber,
+      gender: this.deliveryperson.gender,
+      address: this.address,
+      isActive: this.deliveryperson.isActive,
+    };
+    if (!this.id) {
+      this.deliveryperson.dateJoined = new Date(Date());
+    }
+    console.log(this.deliveryperson);
+    if (this.id) {
+      this.deliveryperson.modifiedDate = new Date(Date());
+      this.deliveryperson.modifiedBy = this.authService.decodedToken.email;
+      this.deliverypersonService.update(this.id, this.deliveryperson).subscribe(
+        (x) => {
+          this.alertify.success('Category updated successfully!');
+          this.router.navigate(['/admin/delivery-person']);
+        },
+        (error) => {
+          this.alertify.error('Category could not be updated!');
+        }
+      );
+    } else {
+      this.deliveryperson.dateAdded = new Date(Date());
+      this.deliveryperson.addedBy = this.authService.decodedToken.email;
+      this.deliveryperson.modifiedDate = new Date(Date());
+      this.deliveryperson.modifiedBy = this.authService.decodedToken.email;
+      this.deliverypersonService.create(this.deliveryperson).subscribe(
+        (x) => {
+          this.alertify.success('Category Added Successfully!');
+          this.router.navigate(['/admin/delivery-person']);
+        },
+        (error) => {
+          this.alertify.error('Category could not be added!');
+        }
+      );
+    }
+  }
+
+  delete() {
+    this.deliverypersonService.delete(this.id).subscribe(
+      (x) => {
+        this.alertify.success('Category deleted successfully!');
+        this.router.navigate(['/admin/delivery-person']);
+      },
+      (error) => {
+        this.alertify.error('Category could not be deleted!');
+      }
+    );
+  }
+
+  openDialog(): void {
+    if (this.id) {
+      let dialogRef = this.dialog.open(ActiveDialogComponent);
+      dialogRef.afterClosed().subscribe((result) => {
+        console.log('Dialog Result: ' + result);
+        if (result == 'Yes') {
+          this.alertify.success('Deliveryman activation status Confirmed.');
+        } else if (result == 'No') {
+          // this.search();
+          this.deliveryperson.isActive = !this.deliveryperson.isActive;
+          this.alertify.warning(
+            'Deliveryman activation status change declined.'
+          );
+        }
+      });
+    }
+  }
 }
