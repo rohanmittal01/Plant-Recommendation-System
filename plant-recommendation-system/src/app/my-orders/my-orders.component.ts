da430ff2fe7fd1bda6e3d0ae665debf59de7e1c8
rohanmittal01 <rohanmittal01@gmail.com>
2020-09-07 03:10:40 +0530

    Modified Check Out, My Orders, Navbar, Shopping Cart

--- plant-recommendation-system/src/app/my-orders/my-orders.component.ts
+++ plant-recommendation-system/src/app/my-orders/my-orders.component.ts
@@ -23,7 +23,8 @@ export class MyOrdersComponent{
   @ViewChild(MatPaginator) paginator: MatPaginator;
   searchKey: string;
   array: any;
-
+  filteredData: any;
+  dateFilterVar: any;
   constructor(private alertify: AlertifyService, private orderService: OrderService) {
     this.orderService.getOrders().subscribe(orders => {
       console.log(orders);
@@ -37,13 +38,51 @@ export class MyOrdersComponent{
       // orders.datePlaced = orders[id].datePlaced.toLocaleString();
       orders[id].datePlaced = new Date(orders[id].datePlaced).toLocaleString();
     }
+    console.log(orders);
+    this.filteredData = orders;
     this.array = orders;
     this.listData = new MatTableDataSource(this.array);
     this.listData.sort = this.sort;
     this.listData.paginator = this.paginator;
    }
+
    applyFilter(){
     this.listData.filter = this.searchKey.toLowerCase();
+  }
+
+  dateFiltering(){
+    console.log(this.dateFilterVar);
+    if(this.dateFilterVar == 'All Days'){
+      this.filteredData = this.array;
+    }else if(this.dateFilterVar == 'last 30 days'){
+      this.filter(30);
+    }else if(this.dateFilterVar ==  'last 180 days'){
+      this.filter(180);
+    }
+
+
+    this.listData = new MatTableDataSource(this.filteredData);
+    this.listData.sort = this.sort;
+    this.listData.paginator = this.paginator;
+  }
+  
+  filter(days){
+    var now = new Date();
+    let yesterday = new Date(new Date().getTime() - (0 * 24 * 60 * 60 * 1000)).toLocaleString();
+    let last7Days = new Date(new Date().getTime() - (days * 24 * 60 * 60 * 1000)).toLocaleString();
+    console.log(yesterday);
+    console.log(last7Days);
+    console.log(this.array.datePlaced);
+    let arr = [];
+      // tslint:disable-next-line: forin
+    for (const id in this.array){
+        // orders.datePlaced = orders[id].datePlaced.toLocaleString();
+        // console.log(this.array[id].datePlaced);
+        if (this.array[id].datePlaced <= yesterday && this.array[id].datePlaced >= last7Days) {
+          arr.push(this.array[id]);
+        }
+      }
+    this.filteredData = arr;
   }
   onSearchClear(){
     this.searchKey = '';
